e3b1e4fe84ceb884234b481eb053c4be34e19b56
Aarya-git25 <ariakatikar@gmail.com>
2026-03-27 12:14:11 +0530

    Update App.java
    public static void main -> public void

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -6,7 +6,7 @@ package com.example;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public void( String[] args )
     {
         System.out.println( "Hello World!" );
     }
